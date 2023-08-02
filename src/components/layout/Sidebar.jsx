d160920cd4852840e770615f09b4c16994de59ea
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-02 16:57:06 +0100

    added my-day nav

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+  Box,
+} from "@mui/material";
+import { FaSun } from "react-icons/fa";
 
+const ListText = styled(ListItemText)({
+  color: "white",
+  marginLeft: "-20px",
+  fontSize: "10px ",
+});
+const Listitem = styled(ListItem)({
+  borderRadius: "5px",
+  "&:hover": { background: "rgb(61 56 56 / .8)" },
+});
 export default function Sidebar() {
   return (
-    <>
-      <Typography variant="caption">Sidebar</Typography>
-    </>
+    <Box pl={0.6} pr={0.6} pt={2}>
+      <List>
+        <Listitem disablePadding={true}>
+          <ListItemButton component="a" href="#">
+            <ListItemIcon sx={{ color: "white" }}>
+              <FaSun fontSize="small" />
+            </ListItemIcon>
+            <ListText primary="My Day" />
+          </ListItemButton>
+        </Listitem>
+      </List>
+    </Box>
   );
 }

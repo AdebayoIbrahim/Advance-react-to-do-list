9f44659e02e587098921b84861dd5c3e1e35a8c2
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-11 18:18:07 +0100

    added adornment

--- src/components/Form/Todoform.jsx
+++ src/components/Form/Todoform.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Box, TextField } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { Add } from "@mui/icons-material";
+import { InputAdornment } from "@mui/material";
 
 const Todoform = () => {
   return (
     <React.Fragment>
       <Box component="form">
         <TextField
-          starticon={<Add />}
           variant="outlined"
           id="input"
           fullWidth
           label="Add Task"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Add sx={{ color: "white" }} />
+              </InputAdornment>
+            ),
+          }}
           sx={{
             "(&:hover)": { background: `${grey[900]} !important` },
           }}

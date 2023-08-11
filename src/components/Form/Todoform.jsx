77d071a0c420161b48eaf7258454b6ab2def3f7b
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-11 16:15:12 +0100

    included input section

--- src/components/Form/Todoform.jsx
+++ src/components/Form/Todoform.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Box, TextField } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { Add } from "@mui/icons-material";
 
 const Todoform = () => {
   return (
     <React.Fragment>
       <Box component="form">
         <TextField
+          starticon={<Add />}
           variant="outlined"
           id="input"
           fullWidth
           label="Add Task"
-          sx={{ outlineColor: "white !important" }}
+          sx={{
+            "(&:hover)": { background: `${grey[900]} !important` },
+          }}
         />
       </Box>
     </React.Fragment>

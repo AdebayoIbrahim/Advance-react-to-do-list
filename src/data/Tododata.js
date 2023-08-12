632ba2f5819de6b8f2640f02ebca406f3b5bda9b
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-12 12:48:46 +0100

    added new todo file

---
+++ src/data/Tododata.js
@@ -0,0 +1,15 @@
+const Todo = [
+  {
+    id: 1,
+    data: "Haging Out around @ 5pm",
+  },
+  {
+    id: 1,
+    data: "Playing Video Games",
+  },
+  {
+    id: 1,
+    data: "Watching Football Mathes!",
+  },
+];
+export default Todo;

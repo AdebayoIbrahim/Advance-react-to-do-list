3d890829ad2d969a8fca843abb8bec25d0a913f9
SetUp <abdulmumeenibroheem51@gmail.com>
2023-08-25 18:17:12 +0100

    added body.jsx starter template

--- src/components/shared/Body.jsx
+++ src/components/shared/Body.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import { Box, Stack } from "@mui/material";
 const Body = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <React.Fragment>
+      <Stack
+        pt={3}
+        pb={3}
+        pl={4}
+        pr={4}
+        direction="column"
+        justifyContent="space-between"
+        sx={{
+          width: "100%",
+          height: "100vh",
+          background: "#212121",
+          backgroundSize: "cover",
+          overflow: "auto !important",
+        }}
+      >
+        {children}
+      </Stack>
+      ;
+    </React.Fragment>
+  );
 };
 
 export default Body;

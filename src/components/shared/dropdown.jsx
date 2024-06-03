179433c15c31d5f3334a5b455282ca7835394cb0
SetUp <abdulmumeenibroheem51@gmail.com>
2024-06-03 16:57:48 +0100

    (feat:/setting current active background status)

--- src/components/shared/dropdown.jsx
+++ src/components/shared/dropdown.jsx
@@ -1,14 +1,14 @@
 import { Box } from "@mui/material";
 import { Typography, Grid } from "@mui/material";
 import bgArr from "../../data/bgArray";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import TodoContext from "../../context/todoContext";
 const DropdownContent = ({ close }) => {
   // calling_defined_context
 
-  const { current, handleBg } = useContext(TodoContext);
+  const { handleBg } = useContext(TodoContext);
   // state-to-toggle-current-clicked
-  //   const [current, setCurrent] = useState();
+  const [current, setCurrent] = useState(`image1`);
 
   // declare_bg_changes
   const handleClick = (payload) => {
@@ -17,6 +17,7 @@ const DropdownContent = ({ close }) => {
 
     // change-current-background
     handleBg(Number(id - 1));
+    setCurrent(`image${id}`);
     // close();
   };
 
@@ -40,7 +41,7 @@ const DropdownContent = ({ close }) => {
                 }}
               >
                 <Box
-                  className={`themes_active ${current}`}
+                  className={`themes_active ${current === `image${bg.id}` ? "act_current" : ""}`}
                   sx={{ width: "65px", height: "65px" }}
                 >
                   <img
